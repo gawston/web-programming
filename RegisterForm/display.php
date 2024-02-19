93b352ab8260f18088b28a29164c281cdc1b5f2c
Naruenat Khamwaree <95065379+gawston@users.noreply.github.com>
2024-02-19 15:58:19 +0700

    Update display.php

--- RegisterForm/display.php
+++ RegisterForm/display.php
@@ -7,29 +7,56 @@
     <title>แสดงผลข้อมูลนักศึกษา</title>
 </head>
 <body>
+    <style>
+        .d {
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
     <?php 
         $fullname = $_GET['full-name'];
         $email = $_GET['email'];
+        $telnumber = $_GET['telnumber'];
         $birthday = $_GET['birthday'];
         $studentid = $_GET['std-id'];
         $eduqualify = $_GET['edu-qualify'];
         $gender = $_GET['gender'];
+
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $dbName = "userdata";
+        $conn = mysqli_connect($hostname, $username, $password);
+        if (!$conn) die("ไม่สามารถติดต่อกับ mySQL ได้");
+        mysqli_select_db($conn, $dbName) or die("ไม่สามารถเลือกฐานข้อมูล bookStore ได้");
+        mysqli_query($conn, "set character_set_connection=utf8mb4");
+        mysqli_query($conn, "set character_set_client=utf8mb4");
+        mysqli_query($conn, "set character_set_results=utf8mb4");
+
+        if(empty($fullname) || empty($email) || empty($telnumber) || empty($studentid)) {
+            echo '<script>alert("กรุณากรอกข้อมูลให้ครบถ้วน"); 
+            history.back(); </script>';
+            exit();
+        } else if(!preg_match('/^[ก-ฯะ-์a-zA-Z ]+$/', $fullname)) {
+            echo '<script>alert("Not use some character"); 
+            history.back(); </script>';
+            exit();
+        } else if(!ctype_digit(trim($telnumber))) {
+            echo '<script>alert("กรุณากรอก หมายเลขโทรศัพท์"); 
+            history.back(); </script>';
+            exit();
+        } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo '<script>alert("กรุณากรอกอีเมลให้ถูกต้อง"); 
+            history.back(); </script>';
+            exit();
+        }
+
+        $sql = "insert into user(userid, username, useremail, userphone, userbirthday, usereducation, usergender) values ('$studentid', '$fullname', '$email', '$telnumber', '$birthday', '$eduqualify', '$gender')";
+        mysqli_query($conn, $sql) or die("Insert ผิดพลาด" . mysqli_error());
+        echo "<div style='display: flex; flex-direction: column;'>";
+        echo "<h1>บันทึกข้อมูลลง database เรียบร้อยแล้ว</h1>";
+        echo "<a href='index.php'><button>กลับหน้าหลัก</button></a>";
+        echo "</div>";
     ?>
-
-    <div class="container">
-        <div class="header">
-            <p>แสดงผลข้อมูลนักศึกษา</p>
-        </div>
-        <div class="content">
-            <p>ชื่อ-นามสกุล: <span><?php echo $fullname ?></span></p>
-            <p>รหัสนักศึกษา: <span><?php echo $studentid ?></span></p>
-            <p>อีเมล: <span><?php echo $email ?></span></p>
-            <p>วัน-เดือน-ปี เกิด: <span><?php echo date_format(date_create($birthday), "d-m-Y") ?></span></p>
-            <p>วุฒิการศึกษา: <span><?php echo $eduqualify ?></span></p>
-            <p>เพศ: <span><?php echo $gender ?></span></p>
-
-            <a href="index.php"><button>กลับหน้าหลัก</button></a>
-        </div>
-    </div>
 </body>
 </html>
